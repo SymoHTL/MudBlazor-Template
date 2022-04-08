e406127901c2d4035c1989bca57d0003967d3498
Symo <simon.hamanek@gmail.com>
2022-04-08 08:31:26 +0200

    default dark mode

--- Domain/Repositories/Implementations/ThemeHandler.cs
+++ Domain/Repositories/Implementations/ThemeHandler.cs
@@ -30,7 +30,27 @@ public class ThemeHandler : IThemeHandler {
 
     public MudTheme Theme { get; set; } = new() {
         Palette = new Palette {
-            Primary = Colors.Green.Default
+            Primary = Colors.Green.Default,
+            Black = "#27272f",
+            Background = "rgb(21,27,34)",
+            BackgroundGrey = "#27272f",
+            Surface = "#212B36",
+            DrawerBackground = "#13171c",
+            DrawerText = "rgba(255,255,255, 0.50)",
+            DrawerIcon = "rgba(255,255,255, 0.50)",
+            AppbarBackground = "#202024",
+            AppbarText = "rgba(255,255,255, 0.70)",
+            TextPrimary = "rgba(255,255,255, 0.70)",
+            TextSecondary = "rgba(255,255,255, 0.50)",
+            ActionDefault = "#adadb1",
+            ActionDisabled = "rgba(255,255,255, 0.26)",
+            ActionDisabledBackground = "rgba(255,255,255, 0.12)",
+            Divider = "rgba(255,255,255, 0.12)",
+            DividerLight = "rgba(255,255,255, 0.06)",
+            TableLines = "rgba(255,255,255, 0.12)",
+            LinesDefault = "rgba(255,255,255, 0.12)",
+            LinesInputs = "rgba(255,255,255, 0.3)",
+            TextDisabled = "rgba(255,255,255, 0.2)"
         },
         LayoutProperties = new LayoutProperties {
             AppbarHeight = "80px",
